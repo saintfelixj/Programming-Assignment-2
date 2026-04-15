786193314339ced519d44731d1cc7ee46f80bcea
saintfelixj <saintfelixj@vcu.edu>
2026-04-15 12:52:34 -0400

    Update fibonacciFilter.js

--- fibonacciFilter.js
+++ fibonacciFilter.js
@@ -4,29 +4,24 @@ const readlineSync = require('readline-sync');
 
 let n = parseInt(readlineSync.question("Enter the number of Fibonacci numbers to generate: "));
 
-if (isNaN(n) || n <= 0) {
-    console.log("Invalid input.");
-} else {
-    let fib = [];
+let fib = [0, 1];
 
-    for (let i = 0; i < n; i++) {
-        if (i === 0) {
-            fib.push(0);
-        } else if (i === 1) {
-            fib.push(1);
-        } else {
-            fib.push(fib[i - 1] + fib[i - 2]);
-        }
-    }
-
-    let oddFib = [];
-
-    for (let i = 0; i < fib.length; i++) {
-        if (fib[i] % 2 !== 0) {
-            oddFib.push(fib[i]);
-        }
-    }
-
-    console.log("Full sequence: [" + fib.join(", ") + "]");
-    console.log("Odd Fibonacci numbers: [" + oddFib.join(", ") + "]");
+// build sequence
+for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
 }
+
+// cut array if n = 1
+fib = fib.slice(0, n);
+
+// filter odd
+let odd = [];
+
+for (let i = 0; i < fib.length; i++) {
+    if (fib[i] % 2 !== 0) {
+        odd.push(fib[i]);
+    }
+}
+
+console.log("Full sequence: [" + fib.join(", ") + "]");
+console.log("Odd Fibonacci numbers: [" + odd.join(", ") + "]");
