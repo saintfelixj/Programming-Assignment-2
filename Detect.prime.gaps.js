fdd968d53871ff33a184f120adbe418608014012
saintfelixj <saintfelixj@vcu.edu>
2026-04-15 12:51:23 -0400

    Add letter frequency counter and first repeat finder

---
+++ Detect.prime.gaps.js
@@ -0,0 +1,39 @@
+Jerell Saint-Felix
+
+const readlineSync = require('readline-sync');
+
+let input = readlineSync.question("Enter a string: ");
+input = input.toLowerCase();
+
+let freq = {};
+let firstRepeat = "";
+
+// count letters
+for (let i = 0; i < input.length; i++) {
+    let ch = input[i];
+
+    if (ch >= 'a' && ch <= 'z') {
+        if (freq[ch] == undefined) {
+            freq[ch] = 1;
+        } else {
+            freq[ch]++;
+        }
+    }
+}
+
+// find first repeat
+for (let i = 0; i < input.length; i++) {
+    let ch = input[i];
+
+    if (ch >= 'a' && ch <= 'z' && freq[ch] > 1) {
+        firstRepeat = ch;
+        break;
+    }
+}
+
+console.log("Letter frequencies:");
+for (let key in freq) {
+    console.log(key + ": " + freq[key]);
+}
+
+console.log("First repeated letter: " + firstRepeat);
